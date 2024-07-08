88261ee38b5d247132bcd88b7052ad90d07a9336
tomasbogalho <tomasbogalho98@gmail.com>
2024-07-08 18:27:30 +0100

    adding template index website to storage container. correct varibales added

--- Backend/main.tf
+++ Backend/main.tf
@@ -6,18 +6,17 @@ resource "azurerm_resource_group" "rg" {
 
 # Creation of the storage account for the static website
 resource "azurerm_storage_account" "sa" {
-  name                          = var.storage_account_name
-  resource_group_name           = azurerm_resource_group.rg.name
-  location                      = azurerm_resource_group.rg.location
-  public_network_access_enabled = true
-  account_tier                  = "Standard"
-  account_replication_type      = "LRS"
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 # Creation of the storage blob for the static website
 resource "azurerm_storage_blob" "example" {
   name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_account_name   = var.storage_account_name
   storage_container_name = "$web"
   type                   = "Block"
   content_type           = "text/html"
