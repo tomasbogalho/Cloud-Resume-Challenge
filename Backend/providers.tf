ebe7ca3ab8f778dbc334603d83d385802f269f88
tomasbogalho <tomasbogalho98@gmail.com>
2024-07-08 15:26:08 +0100

    creation of folder backend and Frontend

---
+++ Backend/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "RG-Management"
+    storage_account_name = "samanagement"
+    container_name       = "terraformstate"
+    key                  = "terraform.tfstate"
+  }
+}
