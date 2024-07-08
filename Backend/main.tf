472cc76ccf3dce6046ffeac1f9f5f69c9339217f
tomasbogalho <tomasbogalho98@gmail.com>
2024-07-08 18:22:56 +0100

    changing storage account access type

--- Backend/main.tf
+++ Backend/main.tf
@@ -6,12 +6,12 @@ resource "azurerm_resource_group" "rg" {
 
 # Creation of the storage account for the static website
 resource "azurerm_storage_account" "sa" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  name                          = var.storage_account_name
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
   public_network_access_enabled = true
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
 }
 
 # Creation of the storage container for the static website
