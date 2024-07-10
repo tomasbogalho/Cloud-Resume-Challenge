3b1f54ecb424a10df3c8be75b65ff28288839e00
tomasbogalho <tomasbogalho98@gmail.com>
2024-07-10 08:40:19 +0100

    adding reserved property to service plan as it is a linux service plan

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -34,6 +34,7 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "Linux"
+  reserved            = true
   sku {
     tier = "Dynamic"
     size = "Y1"
