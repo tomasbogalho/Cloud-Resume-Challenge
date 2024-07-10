e0b790799474269d2fa7e9913630c5bb9c9c17a8
tomasbogalho <tomasbogalho98@gmail.com>
2024-07-10 08:57:10 +0100

    adding a linux app function app instead - correcting. Was using data, but we want the resource...

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -41,7 +41,7 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   }
 }
 # Creation of azure function app
-resource "azurerm_function_app" "functionapp" {
+/*resource "azurerm_function_app" "functionapp" {
   name                       = var.functionapp_name
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
@@ -50,10 +50,18 @@ resource "azurerm_function_app" "functionapp" {
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   version                    = "~4"
 }
+*/
 
-data "azurerm_linux_function_app" "functionapp" {
+resource "azurerm_linux_function_app" "example" {
   name                = var.functionapp_name
   resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
+  service_plan_id            = azurerm_app_service_plan.appserviceplan.id
+
+  site_config {}
 }
 
 output "id" {
